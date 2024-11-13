b74d678fe808c4264badda947ade3d46605dfd17
Yauheni Bykouski <y.bykouski@godeltech.com>
2024-11-13 16:30:32 +0100

    change metadata

--- app/api/contact-us/route.ts
+++ app/api/contact-us/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
     const recipientEmail = process.env.NEXT_PUBLIC_ADMIN_EMAIL || "";
 
     const emailData = await resend.emails.send({
-      from: "no-reply@resend.dev",
+      from: "wartraf.com",
       to: recipientEmail,
       subject: localizedSubject,
       react: React.createElement(EmailTemplate, {
